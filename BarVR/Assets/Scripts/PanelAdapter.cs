61af012cc84392bb08726205e033318f2ab7a8d7
ZHIMIN ZHAO <zhimin@seas.upenn.edu>
2016-01-30 21:28:51 -0500

    with recipe color recurring

---
+++ BarVR/Assets/Scripts/PanelAdapter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PanelAdapter : MonoBehaviour {
+
+    public Color A = Color.magenta;
+    public Color B = Color.blue;
+    public float speed = 1.0f;
+    private Image col;
+    //SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        col = gameObject.GetComponent<Image>();
+        //spriteRenderer = GetComponent<SpriteRenderer> ();
+    }
+
+    void Update()
+    {
+        col.color = Color.Lerp(A, B, Mathf.PingPong(Time.time * speed, 1.0f));
+    }
+}
