ea3497d59a4af659429debe6ba278edeeec85ec9
hubatish <zachariah.howell@gmail.com>
2016-01-30 21:27:44 -0500

    Added health bar prefab

---
+++ BarVR/Assets/Scripts/Health.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Health : MonoBehaviour {
+
+    public static Health Instance;
+
+	void Awake () {
+        Instance = this;
+        curHealth = maxHealth;
+	}
+
+    public Image healthBar;
+    //TODO: make health blood effect around vision
+    //public 
+
+    public float maxHealth = 100;
+
+    protected float curHealth;
+
+    public float deathSpeed = 1f;
+
+    protected void FixedUpdate()
+    {
+        LoseHealth(deathSpeed*Time.deltaTime);
+    }
+
+    public void LoseHealth(float health)
+    {
+        curHealth -= health;
+
+        if (curHealth <= 0)
+        {
+            Lose();
+        }
+
+        healthBar.fillAmount = curHealth / maxHealth;
+    }
+
+    public void Lose()
+    {
+        //TODO: Do cool stuff here
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
